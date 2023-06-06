3028e928dcf5ff852760c99352a04698a2b5cbdf
Zack-hc <zhaci001@gmail.com>
2023-06-06 08:34:26 -0700

    printing by us

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for z in range(0, 100, 1):
-    print("{:02d}".format(z), end='')
-    if z < 99:
-        print(',', end=' ')
+    print("{ :02d}".format(z), end='')
+    if z < 100:
+        print('\n')
