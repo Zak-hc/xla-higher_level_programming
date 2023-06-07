172c99cdffdb893e62469f51ff624a3f8473059d
Zack-hc <zhaci001@gmail.com>
2023-06-06 17:10:43 -0700

    printing by us

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number >= 0):
+        a = number % 10
+    else:
+        a = abs(number) % 10
+    print("{}".format(a), end="")
+    return a
