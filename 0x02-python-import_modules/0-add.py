284533c8ef126ad6f19ae12f99167b1b2e8e553d
Zack-hc <zhaci001@gmail.com>
2023-06-08 05:17:55 -0700

    printing by us

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-ljawab = add(a, b)
-print("{} + {} = {}".format(a, b, ljawab))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    ljawab = add(a, b)
+    print("{} + {} = {}".format(a, b, ljawab))
