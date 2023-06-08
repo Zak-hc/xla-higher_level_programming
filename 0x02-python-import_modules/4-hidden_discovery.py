05df2a7377ecd940190a268c70cbc28b13c079b4
Zack-hc <zhaci001@gmail.com>
2023-06-08 11:46:15 -0700

    printing by us

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import dis
+import hidden_4
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4)]
+    a = len(names)
+    for name in names:
+        if name == 'my_secret_santa' or name == 'print_hidden' or name == 'print_school':
+            print(name)
