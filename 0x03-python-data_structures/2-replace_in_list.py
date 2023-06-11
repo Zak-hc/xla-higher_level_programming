8f56ec4a68250791c000b75ee1754dd7db9f7e61
Zack-hc <zhaci001@gmail.com>
2023-06-11 04:44:09 -0700

    printing by us

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,8 +6,5 @@ def replace_in_list(my_list, idx, element):
     if 0 <= a < len(b):
         b[a] = element
         return(b)
-    elif a < 0 or a >= lena(b):
+    elif a < 0 or a >= len(b):
         return(b)
-
-
-
