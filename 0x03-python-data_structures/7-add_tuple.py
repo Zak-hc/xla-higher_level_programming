cbc7fc5ae5194bd4dbc47c42c5cd176932735de9
Zack-hc <zhaci001@gmail.com>
2023-06-12 16:32:13 -0700

    printing by us

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = (0, 0)
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+
+    if len(tuple_b) < 2:
+        tuple_b = (0, 0)
+    elif len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+    v = ()
+    for i in range(2):
+        v += (tuple_a[i] + tuple_b[i],)
+    return v
