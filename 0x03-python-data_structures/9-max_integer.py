a6137094f5f1fd5000a719bb2fe6791012890e8e
Zack-hc <zhaci001@gmail.com>
2023-06-12 17:51:27 -0700

    printing by us

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    g = 0
+    if my_list == "":
+        print(None)
+    else:
+        for i in range(len(my_list)):
+            if my_list[i] > g:
+                g = my_list[i]
+        return(g)
