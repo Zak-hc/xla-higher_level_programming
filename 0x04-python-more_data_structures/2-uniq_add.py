b5b3f80774313d021256e5e36709f7e37696509d
Zack-hc <zhaci001@gmail.com>
2023-06-14 14:02:15 -0700

    printing by us

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    compa = list(set(my_list)
+    compa = list(set(my_list))
+    resultat = 0
     for i in range(len(compa)):
-        compa[i] += 1
-    return compa[i]
-        
+        resultat += compa[i]
+    return resultat
