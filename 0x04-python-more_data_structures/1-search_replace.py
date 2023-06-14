a35b55979f992c7e0ccc95a56b10e44972916915
Zack-hc <zhaci001@gmail.com>
2023-06-14 10:39:20 -0700

    printing by us

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     my_lista = my_list.copy()
-    my_lista[my_list.index(search)] = replace
+    for idx in range(len(my_lista)):
+        if my_lista[idx] == search:
+            my_lista[idx] = replace
     return my_lista
