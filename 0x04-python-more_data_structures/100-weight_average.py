36b6a1df81f34136ba420f8ed1ea664b07d34185
Zack-hc <zhaci001@gmail.com>
2023-06-14 17:43:45 -0700

    printing by us

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum = 0
+    i = 0
+    j = 0
+    toto_weight = 0
+    if my_list == 0:
+        return 0
+    else:
+        for i, j in my_list:
+            sum += (i * j)
+            toto_weight += j
+            j += 1
+        w = sum/toto_weight
+        return w
