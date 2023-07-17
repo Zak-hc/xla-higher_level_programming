20c8e5bf184f90c766fd8b229802972e09e1979d
Zak.hc <zhaci001@gmail.com>
2023-07-17 13:50:20 -0700

    printing by us

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """ 1-main """
 from models.rectangle import Rectangle
 
@@ -13,4 +12,3 @@ if __name__ == "__main__":
 
     r3 = Rectangle(10, 2, 0, 0, 12)
     print(r3.id)
-
