107423f3e8307c7f5b2cc8f62b53673178052360
Zak.hc <zhaci001@gmail.com>
2023-07-17 12:55:38 -0700

    printing by us

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 from models.base import Base
 class Rectangle(Base):
+    """Represent a rectangle."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """        
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
+
     @property
     def width(self):
+        """Set/get the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -19,9 +37,9 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = value
 
-
     @property
     def height(self):
+        """Set/get the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -34,6 +52,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Set/get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
@@ -46,6 +65,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Set/get the y coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
@@ -56,7 +76,9 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
     def area(self):
+        """Return the area of the Rectangle."""
         return self.width * self.height
     def display(self):
+        """Print the Rectangle using the `#` character."""
         for i in range(self.height):
             print("#" * self.width)
