ae603ea5c97ba37b775a4793d3e1e127c458c4c8
Zack-hc <zhaci001@gmail.com>
2023-08-16 09:07:16 -0700

    printing by us

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- show user permission
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';
