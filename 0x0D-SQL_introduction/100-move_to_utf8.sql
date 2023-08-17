43ec004ee9c7d52d4db54ea10512d931503a0708
Zack-hc <zhaci001@gmail.com>
2023-08-17 11:04:24 -0700

    printing by us

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,5 +6,4 @@ USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the field to utf8mb4
-ALTER TABLE first_table MODIFY  name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER first_table SET name = CONVERT(NAME using utf8mb4);
