161cef2d257434bb9d515003dd5f32f8dd028ec7
Zack-hc <zhaci001@gmail.com>
2023-09-11 16:39:52 -0700

    printing by us

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,22 @@
 #!/usr/bin/node
-const a = process.argv;
-if (!isNaN(a[2])) {
-  add(Number(a[2]));
-} else {
-  console.log('1');
+
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1; // Factorial of NaN or negative numbers is 1
+  } else if (n === 0) {
+    return 1; // Factorial of 0 is 1
+  } else {
+    let result = 1n; // Initialize result as a BigInt
+    for (let i = 1; i <= n; i++) {
+      result *= BigInt(i);
+    }
+    return result;
+  }
 }
-function add (a) {
-  console.log(a * a);
-}
+
+const input = process.argv[2];
+const num = parseInt(input, 10);
+
+const result = factorial(num);
+
+console.log(result.toString());
