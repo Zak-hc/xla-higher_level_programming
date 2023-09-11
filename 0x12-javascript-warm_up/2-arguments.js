e1554b0ab2d2e950c78ea235db5d49b3a60496cb
Zack-hc <zhaci001@gmail.com>
2023-09-11 06:54:44 -0700

    printing by us

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const argumentos = process.argv;
 
-if (argumentos.length > 2) {
+if ( argumentos.length > 3) {
+  console.log('Arguments found');
+} else if (argumentos.length > 2) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('no argument');
 }
