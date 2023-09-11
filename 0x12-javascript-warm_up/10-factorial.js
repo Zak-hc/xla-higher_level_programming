af8562075b0dc93ab29b3b949f2abccd521f1206
Zack-hc <zhaci001@gmail.com>
2023-09-11 14:31:36 -0700

    printing by us

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const a = process.argv;
+if (!isNaN(a[2]) && !isNaN(a[3])) {
+  add(Number(a[2]), Number(a[3]));
+} else {
+  console.log('1');
+}
+function add (a, b) {
+  console.log(a * b);
+}
