c1766de5fdf23fbb86b5060172588e0f80d0c8e8
Zack-hc <zhaci001@gmail.com>
2023-09-11 07:05:31 -0700

    printing by us

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argumentos = process.argv;
 if (argumentos.length > 2) {
-	const vivo = process.argv[2];
-	console.log(vivo);
+  const vivo = process.argv[2];
+  console.log(vivo);
 } else {
   console.log('No argument');
 }
