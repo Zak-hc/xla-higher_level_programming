52efed569b7a6ce78d68669e35dba16fdd7f3762
Zack-hc <zhaci001@gmail.com>
2023-09-11 12:43:23 -0700

    printing by us

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
-let v = process.argv;
-if(!isNaN(v[2]))
-{
-temporino(v);
-}
-else
-{
-console.log('Missing number of occurrences');
+const v = process.argv;
+if (!isNaN(v[2])) {
+  temporino(v);
+} else {
+  console.log('Missing number of occurrences');
 }
 
-function temporino(a)
-{
-let i;
-for(i = 0; i < a[2]; i++)
-{
-console.log('C is fun');
+function temporino (a) {
+  let i;
+  for (i = 0; i < a[2]; i++) {
+    console.log('C is fun');
+  }
 }
-}
-
