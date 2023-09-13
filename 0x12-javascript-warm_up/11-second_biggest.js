27ede9d35b4af25070b1996742957cfd4fe81fed
Zack-hc <zhaci001@gmail.com>
2023-09-13 12:46:06 -0700

    printing by us

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+let a = process.argv.slice(2);
+let s = gr(a);
+
+function gr(s){
+let i;
+let v ,z;
+if(s.length === 0 || s.length === 1)
+{
+console.log(0);
+}
+else{
+for(i = 0; i < s.length -i; i++)
+{
+for(v=0; v < s.length-i -1;v++) 
+{
+if(parseInt(s[v])> parseInt(s[v+1]))
+{
+z = s[v];
+s[v] = s[v+1];
+s[v+1] = z;
+}
+}
+}
+console.log(s[s.length -2]);
+}
+}
