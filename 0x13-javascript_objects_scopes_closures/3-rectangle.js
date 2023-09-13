2126aebf1cd766e1966698d23d9185ab81109ef0
Zack-hc <zhaci001@gmail.com>
2023-09-12 18:37:06 -0700

    printing by us

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+print(){
+const v = 'X'.repeat(this.width);
+for(let i = 0; i < this.height; i++){
+console.log(v);
+}
+}
+}
+module.exports = Rectangle;
