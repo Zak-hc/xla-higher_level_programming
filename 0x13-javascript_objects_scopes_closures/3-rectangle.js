7dd5a151edc2bb4ad0e906bcdae1186d53772d3a
Zack-hc <zhaci001@gmail.com>
2023-09-12 18:55:48 -0700

    printing by us

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
+	constructor (w, h) {
+		if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+			return {};
+		} else {
+			this.width = w;
+			this.height = h;
+		}
+	}
 
-  print () {
-    const v = 'X'.repeat(this.width);
-    for (let i = 0; i < this.height; i++) {
-      console.log(v);
-    }
-  }
+	print () {
+		let i;
+		const v = 'X'.repeat(this.width);
+		for (i = 0; i < this.height; i++) {
+			console.log(v);
+		}
+	}
 }
 module.exports = Rectangle;
