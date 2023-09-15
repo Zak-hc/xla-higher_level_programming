27e76f93b7e779a89f9974835f8061c1e0d04123
Zack-hc <zhaci001@gmail.com>
2023-09-15 10:28:41 -0700

    printing by us

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,27 +3,29 @@ import MySQLdb
 import sys
 
 # Connect to a MySQL database and return the connection and cursor objects.
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-sync = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name)
-cursor = sync.cursor()
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    sync = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name)
+    cursor = sync.cursor()
 
 # Query to select all states from the in ascending order.
-sql_coo = "SELECT * FROM states ORDER BY id ASC"
-cursor.execute(sql_coo)
+    sql_coo = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(sql_coo)
 
 # Fetch all the results.
-lines = cursor.fetchall()
+    lines = cursor.fetchall()
 
 # Display the results.
-for line in lines:
-    print(line)
+    for line in lines:
+        print(line)
 
 # Close the database connection.
-sync.close()
+    sync.close()
