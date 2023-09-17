d17a458c7764d2d807c1be15e4ff4cb509e8f2c3
Zack-hc <zhaci001@gmail.com>
2023-09-17 14:08:39 -0700

    printing by us

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays where name matches"""
+import MySQLdb
+import sys
+
+
+def detectme():
+
+    argvv = sys.argv
+    n = MySQLdb.connect(user=argvv[1],
+                        host='localhost',
+                        port=3306,
+                        passwd=argvv[2],
+                        db=argvv[3])
+
+    veronic = n.cursor()
+    veronic.execute("""SELECT t2.id, t2.name, t1.name
+                    FROM cities AS t2
+                    JOIN states as t1
+                    ON t1.id = t2.state_id ORDER BY id ASC""")
+    hussa = veronic.fetchall()
+    for i in hussa:
+        print(i)
+    n.close()
+
+
+if __name__ == "__main__":
+    detectme()
