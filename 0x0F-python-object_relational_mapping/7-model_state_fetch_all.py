42ff006d3fbdc543924551c305b453a4f6023456
Zack-hc <zhaci001@gmail.com>
2023-09-18 05:35:39 -0700

    printing by us

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 """fiw"""
-
+from sqlalchemy import *
+from model_state import State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+var = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+s = sessionmaker(var)
+session = s()
+rr = session.query(State).all()
+"print(rr)"
+for i in rr:
+    print(i.id, i.name, sep=': ')
