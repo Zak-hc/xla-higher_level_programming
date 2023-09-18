1f797746ccc3573626827ca030b013eb693d0374
Zack-hc <zhaci001@gmail.com>
2023-09-18 07:46:29 -0700

    printing by us

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""fiw"""
+from sqlalchemy import *
+from model_state import State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+if __name__ == "__main__":
+    var = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    s = sessionmaker(var)
+    session = s()
+    rr = session.query(State).filter(State.name.like('%a%')
+                                     ).order_by(State.id.asc()).all()
+    for i in rr:
+        print(i.id, i.name, sep=': ')
