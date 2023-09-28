6903a29bda61291a9e632bfa2fbd9ff8f636aaef
Zack-hc <zhaci001@gmail.com>
2023-09-28 16:18:36 -0700

    task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # cURL body size
-rep=$(curl -s "$1")
-v=0
-for((i = 0; i < ${#rep}; i++)); do
-        v=$((v+1))
-done
 
-echo "${v}"
+v=0; rep=$(curl -s "$1"); for ((i = 0; i < ${#rep}; i++)); do v=$((v+1)); done; echo "${v}"
