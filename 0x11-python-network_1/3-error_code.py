76925871076a55142cf0aa2bd62f19d8c7e332ea
Zack-hc <zhaci001@gmail.com>
2023-09-29 14:39:18 -0700

    printing by us

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,8 +5,7 @@ import sys
 from urllib.error import HTTPError
 try:
     with urllib.request.urlopen(sys.argv[1]) as req:
-        for i in req:
-            if req[i] == 200:
-                print("Index")
+              a = req.read().decode("utf-8")
+              print(a)
 except HTTPError as err:
     print("Error code: ", err.code)
--- 0x11-python-network_1/testurllib.py
+++ 0x11-python-network_1/testurllib.py
@@ -3,3 +3,4 @@ import urllib.request
 p=urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
 print(p)
 print(p.info())
+print(p.read().decode('utf-8'))
