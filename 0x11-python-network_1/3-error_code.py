72d5505709c7b4b7492b14569aa1bbf07a135b4b
Zack-hc <zhaci001@gmail.com>
2023-09-29 14:39:41 -0700

    printing by us

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,7 +5,7 @@ import sys
 from urllib.error import HTTPError
 try:
     with urllib.request.urlopen(sys.argv[1]) as req:
-              a = req.read().decode("utf-8")
-              print(a)
+        a = req.read().decode("utf-8")
+        print(a)
 except HTTPError as err:
     print("Error code: ", err.code)
