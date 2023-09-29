83f7d2fbb119ea4cb4a83dcecefc0aad0e774976
Zack-hc <zhaci001@gmail.com>
2023-09-29 16:33:11 -0700

    printing by us

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3                                                       
-"""lklkkkl"""                                                            
-import requests                                                          
-import sys                                                           
+#!/usr/bin/python3
+"""lklkkkl"""
+import requests
+import sys
 if __name__ == "__main__":
-    x = requests.get(sys.argv[1])                  
+    x = requests.get(sys.argv[1])
     i = 'X-Request-Id'
     stor = x.headers.get(i)
     print(stor)
