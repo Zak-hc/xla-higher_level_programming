e76d4677679d0b6ee76b0f5b216e83fe802399f8
Zack-hc <zhaci001@gmail.com>
2023-09-29 04:54:23 -0700

    printing by us

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
-if __name__ == '__main__':
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as rep:
-        body = rep.read()
-        utf = body.decode('utf-8')
-        print("Body response:")
-        print("    - type:", type(body))
-        print("    - content:", body)
-        print("    - utf8 content:", utf)
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as rep:
+    body = rep.read()
+    utf = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", utf)
