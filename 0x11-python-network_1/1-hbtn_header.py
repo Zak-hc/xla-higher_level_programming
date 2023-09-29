97597d81dbf9e740ecc3718044acaeb1fcb5ff43
Zack-hc <zhaci001@gmail.com>
2023-09-29 05:22:34 -0700

    printing by us

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-#displays the value of the X-Request-Id variable found in the headeroftheresponse
+# displays dvalue of  X-Request-Id variablein the headeroftheresponse
 import sys
 import urllib.request
 with urllib.request.urlopen(sys.argv[1]) as rep:
-        body = rep.info()
-        i = 'X-Request-Id'
-        if i in body:
-            store = body[i]
-            print(store)
-        else:
-            print('desole')
+    body = rep.info()
+    i = 'X-Request-Id'
+    if i in body:
+        store = body[i]
+        print(store)
+    else:
+        print('desole')
