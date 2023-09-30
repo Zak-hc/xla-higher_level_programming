b328bf2975edee780b2d18f126609555e1021432
Zakaria haci <125515540+Zak-hc@users.noreply.github.com>
2023-09-30 01:04:47 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""lklkkkl"""
-import requests
-
-a = requests.get('https://alx-intranet.hbtn.io/status')
-x = a.text
-print("Body response:")
-print("\t- type:", type(x))
-print("\t- content:", x)
--- 0x11-python-network_1/testurllib.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import urllib.request
-p=urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
-print(p)
-print(p.info())
-print(p.read().decode('utf-8'))
