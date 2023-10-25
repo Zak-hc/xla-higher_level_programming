54a241f67d58a8f8cbc3bc7e07932131be3c90c3
Zak-hc <zhaci001@gmail.com>
2023-10-24 18:56:34 -0700

    printing by us

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,10 @@
 const fs = require("fs");
 const stk = process.argv[3];
 const fl = process.argv[2];
+if (!fl || !stk) {
+  console.error('Usage: node script.js <filename> <string>');
+  process.exit(1);
+}
 fs.writeFile(fl, stk, (err) =>
 	{
 	if (err) {
