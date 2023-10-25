8f3201038569b6b2cc58ec397095653bcb1605c6
Zak-hc <zhaci001@gmail.com>
2023-10-24 18:46:46 -0700

    printing by us

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require("fs");
+const stk = process.argv[3];
+const fl = process.argv[2];
+fs.writeFile(fl, stk, (err) =>
+	{
+	if (err) {
+		console.error(err);
+	}
+	});
