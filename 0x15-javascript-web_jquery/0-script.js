d0a3e6da120711a407c1256d12f9a1c5ea58c4c6
Zak-hc <zhaci001@gmail.com>
2023-11-01 17:51:19 -0700

    printing by us

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+function changeclr() {
+    // Select the element with the ID "myElement"
+    var element = document.querySelector("header");
+
+    // Check if the element exists
+    if (element) {
+        // Change the clr  selected element
+        element.style.color == "#FF0000";
